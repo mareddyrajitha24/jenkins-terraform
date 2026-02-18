6a8605986a941d73eb5a730f044b3257ceb83093
Ubuntu <ubuntu@ip-172-31-29-48.eu-north-1.compute.internal>
2026-02-18 10:08:15 +0000

    This is changed one

--- main.tf
+++ main.tf
@@ -2,11 +2,8 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0" # Amazon Linux 2 AMI
+resource "aws_instance" "test" {
+  ami           = "ami-0c55b159cbfafe1f0" # Amazon Linux 2 AMI in us-east-1
   instance_type = "t2.micro"
-
-  tags = {
-    Name = "Terraform-VM"
-  }
 }
+
